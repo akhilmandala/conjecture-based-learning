034c2b0106d95a31fca82642f9d102798e631d00
Akhil Mandala <akhil.mandala@gmail.com>
2020-06-01 23:00:39 -0700

    updated website visuals, added comments

--- javascript/game_simulations.js
+++ javascript/game_simulations.js
@@ -10,11 +10,21 @@ window.addEventListener('load', function initializeGameLoop(event) {
     var isSimulating = true;
     var form = space.getForm();
 
-    var parameters = initializeParameterAndParameterForm();
+    //Load parameters
+    var parameters = initializeParametersAndParameterForm();
+    var gameplayParameters = parameters[0];
+    var visualParameters = parameters[1];
+
     attachListeners();
-    game_loop(parameters);
+    var controller = game_loop(gameplayParameters, visualParameters);
+
+    //TODO: move this function call into a listener
+    loadGameController(controller);
 
     //Helper functions
+
+    //Attaches listeners to buttons and keyboard for toggle events (loading parameters, starting game,
+    //triggering simulations.)
     function attachListeners() {
         //Set parameter values
 
@@ -23,47 +33,78 @@ window.addEventListener('load', function initializeGameLoop(event) {
             loadGameParameters();
         });
 
+        const startGameButton = document.getElementById("start-game");
+        startGameButton.addEventListener('click', function () {
+            console.log("pressed");
+            loadGameController(controller);
+        });
+
         document.addEventListener('keyup', function (event) {
             if (event.ctrlKey && event.key === 't') {
                 event.preventDefault();
                 isSimulating = !isSimulating;
                 console.log(isSimulating)
+            } else if (event.ctrlKey && event.key === 'b') {
+                event.preventDefault();
             }
         });
     }
 
-    function initializeParameterAndParameterForm() {
-        var parameters;
-        if (sessionStorage.getItem("parameters") === null) {
-            console.log("Initializing parameters");
+    //Loads parameters from sessionStorage into variables, and updates fields of parameter forms to those values.
+    //If sessionStorage doesn't contain any data about parameters, the default params are loaded and used instead.
+    function initializeParametersAndParameterForm() {
+        var gameplayParameters, visualParameters;
+        if (sessionStorage.getItem("gameplayParameters") === null) {
             //First time page is being loaded, set the parameters to default
-            sessionStorage.setItem("parameters", JSON.stringify(DEFAULT_GAMEPLAY_PARAMETERS));
+            sessionStorage.setItem("gameplayParameters", JSON.stringify(DEFAULT_GAMEPLAY_PARAMETERS));
         }
 
-        parameters = JSON.parse(sessionStorage.getItem("parameters"));
+        if (sessionStorage.getItem("visualParameters") === null) {
+            sessionStorage.setItem("visualParameters", JSON.stringify(DEFAULT_VISUAL_PARAMETERS));
+        }
 
-        for (let [parameter, value] of Object.entries(parameters)) {
+        gameplayParameters = JSON.parse(sessionStorage.getItem("gameplayParameters"));
+        visualParameters = JSON.parse(sessionStorage.getItem("visualParameters"));
+
+        for (let [parameter, value] of Object.entries(gameplayParameters)) {
             document.getElementById("parameter-" + parameter).setAttribute("value", value);
         }
 
-        console.log("initialized form.");
+        for (let [parameter, value] of Object.entries(visualParameters)) {
+            document.getElementById("parameter-" + parameter).setAttribute("value", value);
+        }
 
-        return parameters;
+        return [gameplayParameters, visualParameters];
     }
 
+    //reloads game and visual parameters upon form submit.
     function loadGameParameters() {
         const parameterForm = document.getElementById("parameter-form");
-        console.log(parameterForm);
         let FD = new FormData(parameterForm);
-        parameters = JSON.parse(sessionStorage.getItem("parameters"));
-        for (var key of FD.keys()) {
-            console.log("key: ", key, " value: ", FD.get(key));
-            parameters[key] = parseFloat(FD.get(key));
+
+        gameplayParameters = JSON.parse(sessionStorage.getItem("gameplayParameters"));
+        visualParameters = JSON.parse(sessionStorage.getItem("visualParameters"));
+        for (var key of Object.keys(gameplayParameters)) {
+            gameplayParameters[key] = parseFloat(FD.get(key));
             document.getElementsByName(key)[0].value = FD.get(key);
         }
-        sessionStorage.setItem("parameters", JSON.stringify(parameters));
+        for (var key of Object.keys(visualParameters)) {
+            gameplayParameters[key] = parseFloat(FD.get(key));
+            document.getElementsByName(key)[0].value = FD.get(key);
+        }
+        sessionStorage.setItem("gameplayParameters", JSON.stringify(gameplayParameters));
+        sessionStorage.setItem("visualParameters", JSON.stringify(visualParameters));
     }
 
+    //TODO: fix loading game controller upon start button
+    //Adds controller object to space. controller object encodes animation and update functions.
+    function loadGameController(controller) {
+        space.add(controller);
+        // bind mouse events and play animation
+        space.bindMouse().bindTouch().play();
+    }
+
+    //Creates a Pts space and canvas.
     function initCanvas() {
         var space = new CanvasSpace("#pt").setup({
             bgcolor: "#345", resize: true, retina: true
@@ -71,6 +112,8 @@ window.addEventListener('load', function initializeGameLoop(event) {
         return space;
     }
 
+    //TODO: map cost function to oscillator better.
+    //Creates an Tone oscillator
     function initOscillator() {
         synth.triggerAttack("C4");
         var osc = new Tone.Oscillator({
@@ -82,23 +125,29 @@ window.addEventListener('load', function initializeGameLoop(event) {
         return osc;
     }
 
+    //TODO: move function and required imports/variables into Game class (makes platform more responsive?)
+    //Generates and returns an IPlayer object for Pts, which encodes update functions and animation functions.
     function game_loop(gameplayParameters = DEFAULT_GAMEPLAY_PARAMETERS, visualParameters = DEFAULT_VISUAL_PARAMETERS) {
+        //Since each player is an object that we add to the space, it's possible we can make each player their own object
+        //could lead to some more interesting dynamics?
+
         // TODO: Link constant parameters to HTML doc
-        const HEIGHT = 600;
-        const WIDTH = 800;
+        const HEIGHT = document.getElementById("gameplay-window-total").offsetHeight;
+        const WIDTH = document.getElementById("gameplay-window-total").offsetWidth;
         const ORIGIN_RADIUS = 10;
         const PLAYER_ACTION_RADIUS = 5;
         const COLOR_P1 = "#e63946";
         const COLOR_P2 = "#ffba08";
 
-        var { p1LearningRate, p2LearningRate, x0, y0, barrier, xLimit, yLimit } = gameplayParameters;
-        var { radius, timeScale, visualScale, outputScale, vectorFieldScale } = visualParameters;
+        var { x0, y0, barrier } = gameplayParameters;
+        var { timeScale, visualScale, vectorFieldScale } = visualParameters;
 
         // Centers of canvas, updated at init
         var yCenter = HEIGHT / 2;
         var xCenter = WIDTH / 2;
         // Starting conditions
-        var x, y;
+        var x = x0;
+        var y = y0;
         // Object to trace taken path
         var history = new Group();
         // Overall game project
@@ -111,32 +160,32 @@ window.addEventListener('load', function initializeGameLoop(event) {
         var xmax = barrier;  // 
         var ymin = -1 * barrier;
         var ymax = barrier;
-        var br1, br2;
 
         //Origin and current action circles
-        var fixed, origin, currentAction;
+        var fixed;
+        var br1, br2;
+        var currentAction;
+        var origin;
 
-        //Vector field
         var vectorfield, vectorfield_pts;
 
-        //Since each player is an object that we add to the space, it's possible we can make each player their own object
-        //could lead to some more interesting dynamics?
+        br1 = Group.fromArray([toScreen(GameState.player1.bestresponse1(ymin), ymin),
+        toScreen(GameState.player1.bestresponse1(ymax), ymax)]);
+        br2 = Group.fromArray([toScreen(xmin, GameState.player2.bestresponse2(xmin)),
+        toScreen(xmax, GameState.player2.bestresponse2(xmax))]);
+
+        fixed = new Pt(GameState.fixedPoint());
+
+        origin = Circle.fromCenter(toScreen(fixed.x, fixed.y), ORIGIN_RADIUS);
+
+        currentAction = Circle.fromCenter(toScreen(x, y), PLAYER_ACTION_RADIUS);
+
         const GameExecutionController = {
             start: (bound) => {
                 y = y0;
                 x = x0;
 
-                br1 = Group.fromArray([toScreen(GameState.player1.bestresponse1(ymin), ymin),
-                toScreen(GameState.player1.bestresponse1(ymax), ymax)]);
-
-                br2 = Group.fromArray([toScreen(xmin, GameState.player2.bestresponse2(xmin)),
-                toScreen(xmax, GameState.player2.bestresponse2(xmax))]);
-
-                fixed = new Pt(GameState.fixedPoint());
-
-                origin = Circle.fromCenter(toScreen(fixed.x, fixed.y), ORIGIN_RADIUS);
-
-                vectorfield = Create.gridPts(space.innerBound, 20, 20).map(
+                vectorfield = Create.gridPts(space.innerBound, 30, 30).map(
                     (p) => {
                         let q, dx, dy, norm;
                         q = fromScreen(p.x, p.y);
@@ -153,7 +202,7 @@ window.addEventListener('load', function initializeGameLoop(event) {
                         return new Group(p, p.$add(new Pt(dx * visualScale, -dy * visualScale)));
                     });
 
-                vectorfield_pts = Create.gridPts(space.innerBound, 20, 20);
+                vectorfield_pts = Create.gridPts(space.innerBound, 30, 30);
             },
 
             animate: (time, ftime) => {
@@ -176,6 +225,7 @@ window.addEventListener('load', function initializeGameLoop(event) {
                 currentAction = Circle.fromCenter(toScreen(x, y), PLAYER_ACTION_RADIUS);
 
                 // tone frequency control => player 1 cost function 
+                // TODO: fix
                 // var f1 = GameState.player1.cost1(x, y);
                 // osc.frequency.rampTo(261 - outputScale * f1, ftime / 1000);
 
@@ -193,12 +243,7 @@ window.addEventListener('load', function initializeGameLoop(event) {
             },
         }
 
-        space.add(GameExecutionController);
-        //Play for 1 minute
-        space.stop(60000);
-
-        // bind mouse events and play animation
-        space.bindMouse().bindTouch().play();
+        return GameExecutionController;
 
         //Helper functions
         // Transformation functions
